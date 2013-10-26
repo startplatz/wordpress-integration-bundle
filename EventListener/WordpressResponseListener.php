ad9bf4e6fb6573db7c5eda93d3d5e2f7b24cf375
Per Bernhardt <info@perprogramming.de>
2013-10-26 12:14:58 +0200

    Bugfix Mobile

--- EventListener/WordpressResponseListener.php
+++ EventListener/WordpressResponseListener.php
@@ -116,7 +116,7 @@ class WordpressResponseListener implements EventSubscriberInterface
     }
 
     protected function expandShortCodes($content) {
-        preg_match('(\<body[^>]+\>(.*)\</body\>)s', $content, $matches);
+        preg_match('(\<body[^>]*\>(.*)\</body\>)s', $content, $matches);
 
         $body = $matches[1];
 
