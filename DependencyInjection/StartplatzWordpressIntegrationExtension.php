b7d87d7cdb8e503f8a49025640da5d4baab1e32b
Gero Duppel <gduppel@pro-volution.de>
2020-05-10 21:48:30 +0200

    Define Services in yaml

--- DependencyInjection/StartplatzWordpressIntegrationExtension.php
+++ DependencyInjection/StartplatzWordpressIntegrationExtension.php
@@ -6,8 +6,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Extension\InvalidArgumentException;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
 
 class StartplatzWordpressIntegrationExtension extends Extension {
@@ -27,12 +26,11 @@ class StartplatzWordpressIntegrationExtension extends Extension {
         }
 
         $wordpressRootDir = realpath($configuration['wordpress_root_dir']);
-        $container->setParameter('startplatz.wordpress_integration.table_prefix', @$configuration['table_prefix'] ?: 'wp_');
         $container->setParameter('startplatz.wordpress_integration.wordpress_root_dir', $wordpressRootDir);
         $container->setParameter('startplatz.wordpress_integration.global_names_cache_file', "$wordpressRootDir/wp-content/globalNames.php");
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
 
     }
 
